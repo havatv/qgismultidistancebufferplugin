ed32ded923037f5cd369cf23056223dc644182e1
havatv <havard.tveite@nmbu.no>
2016-04-22 14:27:46 +0200

    Translation update

--- i18n/nb.ts
+++ i18n/nb.ts
@@ -21,49 +21,54 @@
 <context>
     <name>MultiDistanceBufferDialog</name>
     <message>
-        <location filename="MultiDistanceBuffer_gui.py" line="33"/>
+        <location filename="MultiDistanceBuffer_gui.py" line="26"/>
         <source>MultiDistanceBuffer</source>
         <translation>FlerAvstandsBuffer</translation>
     </message>
     <message>
-        <location filename="MultiDistanceBuffer_gui.py" line="34"/>
+        <location filename="MultiDistanceBuffer_gui.py" line="27"/>
         <source>Cancel</source>
         <translation>Avbryt</translation>
     </message>
     <message>
-        <location filename="MultiDistanceBuffer_gui.py" line="35"/>
+        <location filename="MultiDistanceBuffer_gui.py" line="28"/>
         <source>Close</source>
         <translation>Lukk</translation>
     </message>
     <message>
-        <location filename="MultiDistanceBuffer_gui.py" line="37"/>
+        <location filename="MultiDistanceBuffer_gui.py" line="30"/>
         <source>OK</source>
         <translation>OK</translation>
     </message>
     <message>
-        <location filename="MultiDistanceBuffer_gui.py" line="130"/>
+        <location filename="MultiDistanceBuffer_gui.py" line="129"/>
         <source>MultiDistanceBuffer finished</source>
         <translation>FlerAvstandsBuffer ferdig</translation>
     </message>
     <message>
-        <location filename="MultiDistanceBuffer_gui.py" line="143"/>
+        <location filename="MultiDistanceBuffer_gui.py" line="150"/>
         <source>Aborted</source>
         <translation>Avbrutt</translation>
     </message>
     <message>
-        <location filename="MultiDistanceBuffer_gui.py" line="145"/>
+        <location filename="MultiDistanceBuffer_gui.py" line="152"/>
         <source>No layer created</source>
         <translation>Laget ble ikke laget</translation>
     </message>
     <message>
-        <location filename="MultiDistanceBuffer_gui.py" line="161"/>
-        <source>Worker</source>
-        <translation>Arbeider</translation>
+        <location filename="MultiDistanceBuffer_gui.py" line="122"/>
+        <source>Unable to delete temporary files...</source>
+        <translation>Klarer ikke å slette midlertidige filer...</translation>
     </message>
     <message>
-        <location filename="MultiDistanceBuffer_gui.py" line="167"/>
-        <source>Killing worker</source>
-        <translation>Avbryter arbeider</translation>
+        <location filename="MultiDistanceBuffer_gui.py" line="162"/>
+        <source>Worker failed - exception: </source>
+        <translation>Arbeidsprosessen feilet - avbrudd: </translation>
+    </message>
+    <message>
+        <location filename="MultiDistanceBuffer_gui.py" line="168"/>
+        <source>Worker: </source>
+        <translation>"Arbeidsprosess: </translation>
     </message>
 </context>
 <context>
@@ -89,29 +94,42 @@
         <translation>Bufferavstand</translation>
     </message>
     <message>
-        <location filename="ui_multidistancebuffer.ui" line="77"/>
+        <location filename="ui_multidistancebuffer.ui" line="80"/>
         <source>Add</source>
         <translation>Legg til</translation>
     </message>
     <message>
-        <location filename="ui_multidistancebuffer.ui" line="119"/>
+        <location filename="ui_multidistancebuffer.ui" line="122"/>
         <source>To remove: select by clicking, then press the Remove-button.&lt;br&gt;To add: Enter the distance above and press Enter or the Add-button.</source>
         <translation>For å fjerne: velg ved å klikke, trykk så på Fjern-knappen.&lt;br&gt;For å legge til: Skriv inn avstanden ovenfor og trykk Enter eller Legg til-knappen.</translation>
     </message>
     <message>
-        <location filename="ui_multidistancebuffer.ui" line="132"/>
+        <location filename="ui_multidistancebuffer.ui" line="135"/>
         <source>Remove</source>
         <translation>Fjern</translation>
     </message>
     <message>
-        <location filename="ui_multidistancebuffer.ui" line="153"/>
+        <location filename="ui_multidistancebuffer.ui" line="156"/>
         <source>Use only selected features</source>
         <translation>Benytt bare valgte objekter</translation>
     </message>
     <message>
-        <location filename="ui_multidistancebuffer.ui" line="160"/>
+        <location filename="ui_multidistancebuffer.ui" line="163"/>
         <source>Output (buffer) layer</source>
         <translation>Ut-laget (buffer)</translation>
+    </message>
+</context>
+<context>
+    <name>Worker</name>
+    <message>
+        <location filename="MultiDistanceBuffer_engine.py" line="122"/>
+        <source>Doing buffer distance </source>
+        <translation>Utfører bufferavstand </translation>
+    </message>
+    <message>
+        <location filename="MultiDistanceBuffer_engine.py" line="180"/>
+        <source>Delivering the layer...</source>
+        <translation>Sender over laget...</translation>
     </message>
 </context>
 </TS>
