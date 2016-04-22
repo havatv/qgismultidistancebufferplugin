45d6bbc91cea0a67291b3064cd55bcd5010b6e31
havatv <havard.tveite@nmbu.no>
2016-04-22 14:29:42 +0200

    Add copyright notice

--- MultiDistanceBuffer_gui.py
+++ MultiDistanceBuffer_gui.py
@@ -1,5 +1,24 @@
 # -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ MultiDistanceBuffer_gui
 
+                             -------------------
+        begin                : 2014-09-04
+        git sha              : $Format:%H$
+        copyright            : (C) 2015-2016 by Håvard Tveite
+        email                : havard.tveite@nmbu.no
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 import os
 import glob
 import tempfile
