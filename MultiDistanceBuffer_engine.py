23d672b7b4f227b89f9a3bf03a00b86f8add8221
havatv <havard.tveite@nmbu.no>
2017-04-21 13:39:17 +0200

    Add new field with the inner distance (#17)

--- MultiDistanceBuffer_engine.py
+++ MultiDistanceBuffer_engine.py
@@ -91,6 +91,8 @@ class Worker(QtCore.QObject):
         self.abort = False
         # Distance attribute name
         self.distAttrName = 'distance'
+        # Inner distance attribute name
+        self.innerAttrName = 'inner'
         # Directories and files
         self.tmpbuffbasename = self.tempfilepath + 'outbuff'
         # Options
@@ -112,8 +114,9 @@ class Worker(QtCore.QObject):
             # Remove all the existing attributes:
             while pr.deleteAttributes([0]):
                 continue
-            # Add the distance attribute
+            # Add the distance attributes
             pr.addAttributes([QgsField(self.distAttrName, QVariant.Double)])
+            pr.addAttributes([QgsField(self.innerAttrName, QVariant.Double)])
             layercopy.updateFields()  # Commit the attribute changes
             # Create the memory layer for the results (have to specify a
             # CRS in order to avoid the select CRS dialogue)
@@ -137,6 +140,7 @@ class Worker(QtCore.QObject):
 
             # Do the buffering (order: smallest to largest distances):
             j = 0
+            prevdist = None
             for dist in self.buffersizes:
                 if self.abort is True:
                     break
@@ -188,7 +192,7 @@ class Worker(QtCore.QObject):
                         newgeom = outergeom.symDifference(innergeom)
                     newfeature = QgsFeature()
                     newfeature.setGeometry(newgeom)
-                    newfeature.setAttributes([dist])
+                    newfeature.setAttributes([dist, prevdist])
                     memresult.dataProvider().addFeatures([newfeature])
                 else:
                     # With QGIS3, QgsGeometryAnalyzer causes problems
@@ -240,6 +244,7 @@ class Worker(QtCore.QObject):
                     # Report progress
                     self.calculate_progress()
                 j = j + 1
+                prevdist = dist
             #self.status.emit(self.tr('Finished with buffer ')
             #  + str(datetime.datetime.now().strftime('%H:%M:%S.%f')))
 
